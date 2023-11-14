db58df153f24295c22cdeb393718f4c743f7cf65
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-14 18:30:37 +0200

    added function of sending the result

--- OS_Lab1/src/main/java/clients/Client.java
+++ OS_Lab1/src/main/java/clients/Client.java
@@ -1,17 +1,28 @@
 package clients;
 
+import functions.Functions;
 import org.newsclub.net.unix.AFUNIXSocket;
 import org.newsclub.net.unix.AFUNIXSocketAddress;
 
 import java.io.*;
-
+import java.util.Optional;
 
 public class Client {
     private final File socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "junixsocket-test.sock");
     private Integer x;
 
-    public Client(String funcType) {
+    public Client(String typeOfFunction) {
         readMessage();
+        Optional<Double> result =  Optional.empty();
+        switch (typeOfFunction) {
+            case "F":
+                result = Functions.functionF(x);
+                break;
+            case "G":
+                result = Functions.functionG(x);
+                break;
+        }
+        sendMessage(result, typeOfFunction);
     }
 
     public void readMessage() {
@@ -23,12 +34,27 @@ public class Client {
                 String serverResponse = reader.readLine();
                 System.out.println("Received number from server: " + serverResponse);
                 this.x=Integer.valueOf(serverResponse);
-                System.out.println(x);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void sendMessage(Optional<Double> result, String typeOfFunction){
+        String resultStr = result.map(integer -> typeOfFunction + integer).orElseGet(() -> typeOfFunction + "hard fail");
+
+        try (AFUNIXSocket socket = AFUNIXSocket.newInstance()) {
+            socket.connect( AFUNIXSocketAddress.of(socketFile));
+            try (OutputStream outputStream = socket.getOutputStream()) {
+                String numberToSend = resultStr;
+                outputStream.write(numberToSend.getBytes());
+                System.out.println("Sent result to server: " + numberToSend);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
