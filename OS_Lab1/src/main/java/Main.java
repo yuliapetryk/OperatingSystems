b4c900517752bb9e1b83b385297a562dda515e81
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-14 15:48:13 +0200

    created Manager

---
+++ OS_Lab1/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException, TimeoutException {
+        Manager manager = new Manager();
+    }
+}
--- OS_Lab1/src/main/java/Manager.java
+++ OS_Lab1/src/main/java/Manager.java
@@ -1,7 +1,24 @@
+import java.io.*;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+
+import org.newsclub.net.unix.AFUNIXServerSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
 
 public class Manager {
-    public static void main(String[] args) {
+    private static  Integer x;
+    private static Executor executor;
+    private static Process processF;
+    private static Process processG;
+    private static AFUNIXServerSocket server;
+
+    public Manager() throws IOException, InterruptedException, TimeoutException {
         Scanner sc = new Scanner(System.in);
         int x = 0;
         System.out.println("Please, enter x: ");
@@ -9,6 +26,32 @@ public class Manager {
             x = sc.nextInt();
         }
         sc.nextLine();
-        System.out.println(x);
+        this.x = x;
+        runManager();
     }
+
+    public static void runManager() throws IOException, InterruptedException, TimeoutException {
+        File socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "junixsocket-test.sock");
+        try (AFUNIXServerSocket serverSocket = AFUNIXServerSocket.newInstance()) {
+            serverSocket.bind( AFUNIXSocketAddress.of(socketFile));
+            System.out.println("Server is running. Waiting for connections...");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected");
+
+                try (OutputStream outputStream = clientSocket.getOutputStream()) {
+                    outputStream.write(Integer.toString(x).getBytes());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
+
